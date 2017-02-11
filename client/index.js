0c315d071db44fa03742c50da6ee7e0adc5dc521
edsant1 <edwins10329@gmail.com>
2017-02-10 17:30:21 -0700

    installed and added React router for my routes.

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
 import App from './containers/App';
+import routes from './routes';
 
-ReactDOM.render(<App />, document.getElementById('content'));
+ReactDOM.render(<Router history={browserHistory} routes={routes} />, document.getElementById('content'));
