601f6ca89b567a4c60f3620333250d2a398502ac
edsant1 <edwins10329@gmail.com>
2017-03-14 18:26:33 -0600

    working on footer got a good start working on social media logos

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-const Footer = () => (
-	<footer className="ft-bkgrd">
-		<div className="contactDiv">
-			<h4>Contact Us</h4>
-			<p>Call or text: 801.668.5796</p>
-			<p>Email: herb84054@gmail.com</p>
-			<div className="cp-cby">
-				<p>Website by CK</p>
+class Footer extends React.Component {
+
+	render() {
+		return (
+			<div className="row ft-bkgrd">
+				<div className="contactDiv col m5 s12">
+					<h4 style={{marginBottom: '1.4rem', fontSize: '5rem', fontWeight:'300'}}>Contact Us</h4>
+					<p>Call or text: 801.668.5796</p>
+					<p>Email: herb84054@gmail.com</p>
+				</div>
+				<div className="logoDiv col m3 s6 offset-m2">
+					<img src={'socialMediaLogos/FB-f-Logo__white_58.png'} />
+				</div>
+				<div className=" col m12 s12 cp-cby">
+					<p>Website by CK</p>
+				</div>
 			</div>
-		</div>
-	</footer>
-)
+		)
+	}
+}
 
 export default Footer;
